72f394b8e3f344812289d4ac26aa2529442c8822
Gameramirali <118621907+Gameramirali@users.noreply.github.com>
2025-03-01 16:55:27 -0800

    a

---
+++ school/python/home work/exam.py
@@ -0,0 +1,25 @@
+# 1-1
+# name=input('enter your name: ')
+# print(name[::-1])
+# 1-2
+# name="alireza"
+# finally1=""
+# for i in name:
+#     finally1 = i + finally1
+
+# print(finally1)
+
+# 2
+
+# 1
+# 1 2
+# 1 2 3 
+# 1 2 3 4
+# 1 2 3 4 5
+
+# x=1
+# y=''
+# for i in range(1,6):
+#     print(f'{y}{str(x)}')
+#     y+=str(x)+' '
+#     x+=1
